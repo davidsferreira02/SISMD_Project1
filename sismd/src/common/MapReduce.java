d62306afb6fca8f74c3c4ad6baa1471f34ffd968
IsepMartimOliveira <106027678+IsepMartimOliveira@users.noreply.github.com>
2025-05-05 02:05:46 +0100

    Update code: Added MapReduce

---
+++ sismd/src/common/MapReduce.java
@@ -0,0 +1,31 @@
+package common;
+
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class MapReduce {
+
+    private final ConcurrentMap<String, Integer> wordCounts = new ConcurrentHashMap<>();
+
+    public void map(String text) {
+        for (String word : text.split("\\W+")) {
+            if (!word.isEmpty()) {
+                String lower = word.toLowerCase();
+                wordCounts.merge(lower, 1, Integer::sum);
+            }
+        }
+    }
+
+    public ConcurrentMap<String, Integer> getCounts() {
+        return wordCounts;
+    }
+
+    public void printTopWords(int topN) {
+        wordCounts.entrySet().stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .limit(topN)
+                .forEach(entry ->
+                        System.out.println("Word: '" + entry.getKey() + "' occurred " + entry.getValue() + " times!"));
+    }
+}
